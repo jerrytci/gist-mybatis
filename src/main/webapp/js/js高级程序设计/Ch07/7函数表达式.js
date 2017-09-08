ecd52fd458d9ef4876da9f760eb1e3c3f9848f95
jerry.chen <jerry.chen@astasys.com>
2017-09-08 18:31:26 +0800

    js高级程序设计：7 闭包。

--- src/main/webapp/js/js高级程序设计/Ch07/7函数表达式.js
+++ src/main/webapp/js/js高级程序设计/Ch07/7函数表达式.js
@@ -50,3 +50,44 @@ var factorial3 = (function f(num) {
 // console.log(f.name);  // f is not defined.
 console.log(factorial3(4));  //24
 
+console.log("7.2 闭包");
+console.log("闭包：指有权访问另一个函数作用域中的变量的函数");
+
+function createComparisonFunction(propertyName) {
+    return function(object1, object2){
+        var value1 = object1[propertyName];
+        var value2 = object2[propertyName];
+
+        if (value1 < value2){
+            return -1;
+        } else if (value1 > value2){
+            return 1;
+        } else {
+            return 0;
+        }
+    };
+}
+
+var c = createComparisonFunction("name");
+var result = c({name: "Nicholas"}, {name: "Greg"});
+console.log(result);
+// c = null;
+
+
+console.log("7.2.1 闭包和变量");
+function createFunction() {
+    var result = new Array();
+    for (var i = 0; i < 4; i++) {
+        result[i] = function () {
+            return i;
+        }
+    }
+    return result;
+}
+var c2 = createFunction();
+for(i in c2){
+   var result2 = c2[i];
+    console.log(result2(5))
+}
+
+
