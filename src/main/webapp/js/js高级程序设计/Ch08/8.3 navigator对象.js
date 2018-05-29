c1885335480a90cb5db1d2982a2c1d05116a2e20
jerry.chen <jerry.chen@astasys.com>
2018-05-29 12:16:18 +0800

    重构

--- src/main/webapp/js/js高级程序设计/Ch08/8.3 navigator对象.js
+++
@@ -1,4 +0,0 @@
-console.log("8.3 navigator对象");
-console.log("检测浏览器类型");
-console.log("8.3.1 检测插件");
-console.log("8.3.2 注册处理程序");
--- src/main/webapp/js/js高级程序设计/Ch08/8.4 screen对象.js
+++
@@ -1,2 +0,0 @@
-console.log("8.4 screen对象");
-console.log("获取浏览器窗口外部的显示器的信息");
